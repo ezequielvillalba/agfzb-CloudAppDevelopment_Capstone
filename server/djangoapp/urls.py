28179434061598945a2ed51c6491ca97f8cbbf11
ezequielvillalba <116312245+ezequielvillalba@users.noreply.github.com>
2022-10-25 18:00:48 -0300

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     # path for add a review view
     path('add_review/<int:dealerId>/', views.add_review, name='add_review')   
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
