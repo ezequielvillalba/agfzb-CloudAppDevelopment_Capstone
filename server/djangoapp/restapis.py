626e0d4929b667664f55e98cd9cd92a74298a3ec
ezequielvillalba <116312245+ezequielvillalba@users.noreply.github.com>
2022-10-25 22:58:10 -0300

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -168,17 +168,18 @@ def get_dealer_reviews_from_cf(url, dealer_id):
 # - Get the returned sentiment label such as Positive or Negative
 
 
-def analyze_review_sentiments(text):
-    api_key = "Tis_kkoet1gdM43W9j8hFT6IUe22ymwtFCx3LJFVy4Cw"
-    url = "https://322d2895-8806-458a-992a-18f2fb8cd10a-bluemix.cloudantnosqldb.appdomain.cloud"
-    texttoanalyze = text
-    version = '2020-08-01'
-    authenticator = IAMAuthenticator(api_key)
-    natural_language_understanding = NaturalLanguageUnderstandingV1(
-        version='2021-08-01', authenticator=authenticator)
-    natural_language_understanding.set_service_url(url)
-    response = natural_language_understanding.analyze(text=text, features=Features(
-        sentiment=SentimentOptions(targets=[text])), language='en').get_result()
-    label = json.dumps(response, indent=2)
-    label = response['sentiment']['document']['label']
-    return(label)
+
+def analyze_review_sentiments(review):
+    authenticator = IAMAuthenticator('Tis_kkoet1gdM43W9j8hFT6IUe22ymwtFCx3LJFVy4Cw')
+    nlu = NaturalLanguageUnderstandingV1(
+        version='2022-04-07', 
+        authenticator=authenticator
+        )
+    nlu.set_service_url('https://322d2895-8806-458a-992a-18f2fb8cd10a-bluemix.cloudantnosqldb.appdomain.cloud')
+
+    response = nlu.analyze(text = review, 
+    features=Features(sentiment=SentimentOptions(document = True))).get_result()
+
+    #print(json.dumps(response['sentiment']['document']['label'], indent=2))
+
+    return response['sentiment']['document']['label']
