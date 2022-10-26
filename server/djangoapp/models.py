7fa04f029e427729df971d2efd1a75eeed22abbd
ezequielvillalba <116312245+ezequielvillalba@users.noreply.github.com>
2022-10-25 23:02:10 -0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,34 +6,52 @@ import uuid
 import json
 # Create your models here.
 
+
+# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# - Name
+# - Description
+# - Any other fields you would like to include in car make model
+# - __str__ method to print a car make object
+
 class CarMake(models.Model):
-    name = models.CharField(max_length=30, default='car make')
-    description = models.CharField(max_length=1000, default='car make description')
-
+    maker_name = models.CharField(null=False, max_length=30, default='BMW')
+    description = models.CharField(null=False, max_length=500)
+    #dob = models.DateField(null=True)
+    
+    # Create a toString method for object string representation
     def __str__(self):
-        return "Name: " + self.name + "," + \
-               "Description: " + self.description
+        return "Name: " + self.maker_name
 
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+# - Name
+# - Dealer id, used to refer a dealer created in cloudant database
+# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+# - Year (DateField)
+# - Any other fields you would like to include in car model
+# - __str__ method to print a car make object
 class CarModel(models.Model):
-    SEDAN = 'SEDAN'
+    
     SUV = 'SUV'
-    WAGON = 'WAGON'
-    TYPE_CHOICES = (
-        (SEDAN, "SEDAN"),
-        (SUV, "SUV"),
-        (WAGON, "WAGON")
-    )
-    dealer_id = models.IntegerField(default=0)
-    name = models.CharField(max_length=30, default='car model')
-    type = models.CharField(max_length=7, choices=TYPE_CHOICES, default=SEDAN)
-    year = models.DateField(default=now)
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    SEDAN = 'Sedan'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    TYPE_CHOICES = [
+        (SUV, 'SUV'),
+        (SEDAN, 'Sedan'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe')]
+    
+    model_name = models.CharField(null=False, max_length=30, default='X7')
+    dealerID = models.IntegerField(default=1,primary_key=True)
+    car_type = models.CharField(null=False,max_length=30, choices=TYPE_CHOICES, default=SEDAN)
+    year = models.DateField(null=True)
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 
+    
+    # Create a toString method for object string representation
     def __str__(self):
-        return "Name: " + self.name + "," + \
-               "DealerId: " + self.dealer_id.__str__() + "," + \
-               "Type: " + self.type + "," + \
-               "Year: " + self.year.__str__()
+        return "Name: " + self.model_name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
