c9ea9f35bf93ac8936ff9fa0f219e05e388beade
ezequielvillalba <116312245+ezequielvillalba@users.noreply.github.com>
2022-10-27 20:05:30 -0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,43 +1,47 @@
 
 from django.db import models
 from django.utils.timezone import now
-from django.contrib import admin
-from django.core import serializers 
-import uuid
-import json
+
+
 # Create your models here.
 
+# Car Make model:
 class CarMake(models.Model):
-    name = models.CharField(max_length=30, default='car make')
-    description = models.CharField(max_length=1000, default='car make description')
+    name = models.CharField(null=False, max_length=30, default='car make')
+    description = models.CharField(max_length=1000)
 
     def __str__(self):
         return "Name: " + self.name + "," + \
                "Description: " + self.description
 
+
+# Car Model model:
 class CarModel(models.Model):
-    SEDAN = 'SEDAN'
+    SEDAN = 'Sedan'
     SUV = 'SUV'
     WAGON = 'WAGON'
-    TYPE_CHOICES = (
-        (SEDAN, "SEDAN"),
-        (SUV, "SUV"),
-        (WAGON, "WAGON")
-    )
-    dealer_id = models.IntegerField(default=0)
-    name = models.CharField(max_length=30, default='car model')
-    type = models.CharField(max_length=7, choices=TYPE_CHOICES, default=SEDAN)
-    year = models.DateField(default=now)
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    SPORTS_CAR = 'Sports car'
+    CAR_MODEL_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON'),
+        (SPORTS_CAR, 'Sports car')
+    ]
 
+    CarMake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30, default='car model')
+    id =  models.IntegerField(default=0, primary_key=True)
+    car_model = models.CharField(max_length=10, choices=CAR_MODEL_TYPES, default=SEDAN)
+    car_year = models.DateField(default=now)
+    
     def __str__(self):
         return "Name: " + self.name + "," + \
-               "DealerId: " + self.dealer_id.__str__() + "," + \
-               "Type: " + self.type + "," + \
-               "Year: " + self.year.__str__()
+               "Model: " + self.car_model
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+
+# Python class `CarDealer`
 class CarDealer:
+
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
         # Dealer address
         self.address = address
@@ -62,7 +66,6 @@ class CarDealer:
         return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
-# class DealerReview:
 class DealerReview:
     def __init__(self, dealership, name, purchase, review):
         self.dealership = dealership
