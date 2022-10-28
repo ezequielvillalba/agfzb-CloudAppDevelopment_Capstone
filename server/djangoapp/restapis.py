c3c455456666ba764594456f0e8ac6e34e6cad33
ezequielvillalba <116312245+ezequielvillalba@users.noreply.github.com>
2022-10-28 15:48:57 -0300

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -170,10 +170,10 @@ def get_dealer_reviews_from_cf(url, **kwargs):
 
 def analyze_review_sentiments(dealerReview, language="en"):
     try:
-        #url = "https://322d2895-8806-458a-992a-18f2fb8cd10a-bluemix.cloudantnosqldb.appdomain.cloud"
-        #api_key = "Tis_kkoet1gdM43W9j8hFT6IUe22ymwtFCx3LJFVy4Cw"
-        url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/90de55ad-a32d-44dc-af61-04d63b75e30e"
-        api_key = "kzqDTUcGhbvdUsI_QCnhApNTT-nPGNb4ZGlw2F2Jvkte"
+        url = "https://322d2895-8806-458a-992a-18f2fb8cd10a-bluemix.cloudantnosqldb.appdomain.cloud"
+        api_key = "Tis_kkoet1gdM43W9j8hFT6IUe22ymwtFCx3LJFVy4Cw"
+        #url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/90de55ad-a32d-44dc-af61-04d63b75e30e"
+        #api_key = "kzqDTUcGhbvdUsI_QCnhApNTT-nPGNb4ZGlw2F2Jvkte"
         authenticator = IAMAuthenticator(api_key)
         natural_language_understanding = NaturalLanguageUnderstandingV1(version='2021-08-01',authenticator=authenticator)
         natural_language_understanding.set_service_url(url)
